e3c3a61c415be1c596188b0565d88bfc78f2826e
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2019-12-03 15:22:58 -0800

    MVP completed

--- js/index.js
+++ js/index.js
@@ -46,6 +46,81 @@ for (let pId = 0; pId < paragraphText.length; pId ++)
 let bus = document.createElement("img");
 bus.src = "img/fun-bus.jpg";
 
-let boatDiv = document.querySelector(".content-destination");
-boatDiv.addEventListener("wheel", () => boatDiv.appendChild(bus));
+let imgAdded = false;
 
+let boatDiv = document.querySelector(".content-destination");
+boatDiv.addEventListener("click", () => {
+
+    if (imgAdded)
+        {
+            bus.remove();
+            imgAdded = false;
+        }
+    else
+        {
+            boatDiv.appendChild(bus);
+            imgAdded = true;
+        }
+});
+
+// change background color on keypress
+let destinationText = document.querySelector(".content-destination p");
+let keyPressCount = {red: 0, green: 0, blue: 0, gray: 0};
+
+document.addEventListener("keydown", () => {
+    destinationText.style.backgroundColor = calculateColor(keyPressCount, "gray");
+} )
+
+// function to change color based on keypresses
+function calculateColor(keypressCount, color) {
+
+    keyPressCount[color] += 1;
+
+    if (keyPressCount[color] > 15)
+        { keyPressCount[color] = 0;}
+
+    let codes = "0123456789ABCDEF";
+    let hexCode = codes[keypressCount[color]] + codes[keypressCount[color]];
+
+    if (color === "red")
+        { return "#" + hexCode + "0000"; }
+    else if (color === "green")
+        { return "#00" + hexCode + "00"; }
+    else if (color === "blue")
+        { return "#0000" + hexCode; }
+    else
+        { return "#" + hexCode + hexCode + hexCode; }
+}
+
+// change background color on keypress, keyup, and keydown
+let destinationParagraphs = document.querySelectorAll(".destination p");
+
+document.addEventListener("keypress", () => {
+    destinationParagraphs[0].style.backgroundColor = calculateColor(keyPressCount, "red");
+
+    destinationParagraphs[0].innerHTML += "<em>keypress</em>! ";
+} )
+
+document.addEventListener("keyup", () => {
+    destinationParagraphs[1].style.backgroundColor = calculateColor(keyPressCount, "green");
+
+    destinationParagraphs[1].innerHTML += "keyup</em>! ";
+} )
+
+document.addEventListener("keydown", () => {
+    destinationParagraphs[2].style.backgroundColor = calculateColor(keyPressCount, "blue");
+    
+    destinationParagraphs[2].innerHTML += "<em>keydown</em>! ";
+} )
+
+// change background color on click - stopping propagation
+let h1 = document.querySelector("h1");
+h1.addEventListener("click", (element) => {
+    h1.style.backgroundColor = "#CC9966";
+    event.stopPropagation();
+});
+
+let header = document.querySelector("header");
+header.addEventListener("click", () => header.style.backgroundColor = "#FFCC99")
+
+// stop navigation items from refreshing page
